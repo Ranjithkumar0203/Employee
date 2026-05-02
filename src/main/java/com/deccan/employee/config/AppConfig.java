539384556ac6ef25a9f5c87bcb1f4729cc7f583b
Ranjithkumar0203 <ranjith.kumar03@gmail.com>
2026-05-02 22:24:38 +0530

    uiBeanCommit

--- src/main/java/com/deccan/employee/config/AppConfig.java
+++ src/main/java/com/deccan/employee/config/AppConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -21,5 +23,18 @@ public class AppConfig {
                         .title("Employee API")
                         .description("API documentation for testing employee endpoints")
                         .version("v1"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/employees/**")
+                        .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
